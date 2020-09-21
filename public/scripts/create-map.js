0da93329b80cc41792e67c78407a7ec95fce7056
cplpearce <clint.pearce@rocketmail.com>
2020-09-21 10:56:26 -0400

    Add user settings/map creation html

--- public/scripts/create-map.js
+++ public/scripts/create-map.js
@@ -8,13 +8,14 @@ $( document ).ready(function() {
       center: new L.LatLng(43.6532, -79.3832),
       zoom: 12
   });
+  L.DomUtil.addClass(map._container,'marker-cursor-enabled');
   map.addLayer(layer);
 
   map.on('click', (event) => {
     const coord = event.latlng.toString().split(',');
-    const lat = coord[0].split('(');
-    const lon = coord[1].split(')');
-    alert(`You clicked the map at latitude ${lat[1]} and longitude ${lon[0]}`);
+    const clickLat = coord[0].split('(');
+    const clickLon = coord[1].split(')');
+    alert(`You clicked the map at latitude ${clickLat[1]} and longitude ${clickLon[0]}`);
     // TODO: Create a server function to pass the point to the DB
   });
   // Without this, the map won't re-size if the display is set to none||hidden
