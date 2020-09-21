091b6ad6b8beb1c5ac0d0e9ba421bab803c973dd
cplpearce <clint.pearce@rocketmail.com>
2020-09-21 14:07:19 -0400

    Add SPA integration to Create Map page

--- public/scripts/create-map.js
+++ public/scripts/create-map.js
@@ -6,26 +6,43 @@ $( document ).ready(function() {
   const layer = new L.StamenTileLayer("toner");
   const map = new L.Map("map-frame", {
       center: new L.LatLng(43.6532, -79.3832),
-      zoom: 12
+      zoom: 12,
+      name: 'My new Map',
   });
   L.DomUtil.addClass(map._container,'marker-cursor-enabled');
+  // Disable map zoom double clicks
+  map.doubleClickZoom.disable();
   map.addLayer(layer);
   // Make sure our toolbar is clickable
   const toolbar = L.DomUtil.get('create-map-toolbar');
-
   // Don't drop a point if user clicks the toolbar
   L.DomEvent.on(toolbar, 'mousewheel', L.DomEvent.stopPropagation);
-  L.DomEvent.on(toolbar, 'click', L.DomEvent.stopPropagation);
+  L.DomEvent.on(toolbar, 'mouseup', L.DomEvent.stopPropagation);
 
-  map.on('click', (event) => {
+  map.on('dblclick', (event) => {
     const coord = event.latlng.toString().split(',');
-    const clickLat = coord[0].split('(');
-    const clickLon = coord[1].split(')');
-    alert(`You clicked the map at latitude ${clickLat[1]} and longitude ${clickLon[0]}`);
+    const marker = new L.marker(event.latlng, {type: 'marker', draggable: true}).addTo(map);
     // TODO: Create a server function to pass the point to the DB
   });
   // Without this, the map won't re-size if the display is set to none||hidden
   $( '#nav-buttons' ).children().click(() => {
     map.invalidateSize();
   });
+  $( '#map-points-btn' ).click(function() {
+    $( '#map-points-table-body' ).empty();
+    let count = 1;
+    map.eachLayer(function (layer) {
+      if (layer.options.type === 'marker') {
+        $( '#map-points-table-body' ).append(`
+        <tr>
+        <th scope="row">${count}</th>
+        <td>${layer._latlng.lat.toFixed(5)}</td>
+        <td>${layer._latlng.lng.toFixed(5)}</td>
+        </tr>
+        `);
+        count += 1;
+      }
+    });
+  })
 });
+
